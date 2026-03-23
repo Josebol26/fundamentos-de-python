6856e0050d7fa08ea2bc3bea698e3382276493c7
Jose Fonseca <jose9673486@edu.pa.senac.br>
2026-03-23 20:55:51 -0300

    aula do dia 23
    aula do dia 23

---
+++ Operações.py
@@ -0,0 +1,14 @@
+var1 = 2
+var2 = 3
+soma = var1 + var2
+print("O resultado da soma é",soma)
+multiplicacao = var1 * var2
+print("O resultado da multiplicação",multiplicacao)
+subtracao = var2 - var1
+print("O resultado da subtração é",subtracao)
+divisao = var2/var1
+print("O resultado da divisão é",divisao)
+exponenciacao = var1 ** var2
+print("O resultado da exponenciação é",exponenciacao)
+ExpressaoNumerica = ((var1 * var2 * (var1 - var2)) / ((var1 ** var2 + var1)))
+print("O resultado da Expressão Numérica é",ExpressaoNumerica)
---
+++ tipos.py
@@ -0,0 +1,22 @@
+#Variáveis
+var = "João"
+print(var)
+var = "José"
+print(var)
+var = 15
+print(var)
+var = "15"
+print(var)
+
+
+#Tipos de dados
+#Inteiro
+inteiro = 1
+#Fracionário/Float
+fracionário = 1.0
+#Textos (String)
+texto = "Exemplo"
+#Lógico (Boolean)
+logico = True
+#Vazio
+vazio = None
